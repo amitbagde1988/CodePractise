2731c122e4033f4e3611e5efb9045915cba5f67b
amitbagde1988 <87758625+amitbagde1988@users.noreply.github.com>
2021-07-30 11:34:45 +0530

    Add files via upload

---
+++ Test.ProdHouse/AllMovies.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Test.ProdHouse.Models;
+
+namespace Test.ProdHouse
+{
+    public partial class AllMovies : Form
+    {
+        public AllMovies()
+        {
+            InitializeComponent();
+        }
+
+        private void AllMovies_Load(object sender, EventArgs e)
+        {
+            List<Movie> _movies = Business.MovieInfo.GetAll();
+            dataGridView1.DataSource = _movies.Select(p => new { p.ID, p.Name, p.Language }).ToList();
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            List<Movie> _movies = Business.MovieInfo.GetAll();
+            Parallel.ForEach(_movies, _movie =>
+            {
+                Business.MovieInfo.Print(_movie);
+                this.Invoke(new MethodInvoker(delegate () {
+                    lblLogs.Text += $"\r\n Print {_movie.Name}";
+                }));
+            });
+        }
+    }
+}
---
+++ Test.ProdHouse/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace Test.ProdHouse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMovieName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.drpLanguage = new System.Windows.Forms.ComboBox();
+            this.chkGenre = new System.Windows.Forms.CheckedListBox();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.txtBudget = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.grpFormats = new System.Windows.Forms.GroupBox();
+            this.grpTypes = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProviderApp = new System.Windows.Forms.ErrorProvider(this.components);
+            this.grpFormats.SuspendLayout();
+            this.grpTypes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderApp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Movie Name";
+            // 
+            // txtMovieName
+            // 
+            this.txtMovieName.Location = new System.Drawing.Point(101, 13);
+            this.txtMovieName.Name = "txtMovieName";
+            this.txtMovieName.Size = new System.Drawing.Size(199, 20);
+            this.txtMovieName.TabIndex = 1;
+            this.txtMovieName.TextChanged += new System.EventHandler(this.txtMovieName_TextChanged);
+            this.txtMovieName.Validating += new System.ComponentModel.CancelEventHandler(this.txtMovieName_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Type";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 17);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "4D";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(51, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(41, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "HD";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(98, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(51, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "IMAX";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Budget (In Million)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Movie Length";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 198);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Movie Format";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 260);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Description";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(16, 330);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Language";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 374);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(36, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Genre";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(16, 455);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(94, 40);
+            this.btnSubmit.TabIndex = 12;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // txtDescription
+            // 
+            this.txtDescription.Location = new System.Drawing.Point(101, 260);
+            this.txtDescription.Multiline = true;
+            this.txtDescription.Name = "txtDescription";
+            this.txtDescription.Size = new System.Drawing.Size(330, 57);
+            this.txtDescription.TabIndex = 13;
+            // 
+            // drpLanguage
+            // 
+            this.drpLanguage.DisplayMember = "English";
+            this.drpLanguage.FormattingEnabled = true;
+            this.drpLanguage.Items.AddRange(new object[] {
+            "Hindi",
+            "English",
+            "German",
+            "Korean"});
+            this.drpLanguage.Location = new System.Drawing.Point(101, 330);
+            this.drpLanguage.Name = "drpLanguage";
+            this.drpLanguage.Size = new System.Drawing.Size(121, 21);
+            this.drpLanguage.TabIndex = 14;
+            this.drpLanguage.ValueMember = "English";
+            // 
+            // chkGenre
+            // 
+            this.chkGenre.FormattingEnabled = true;
+            this.chkGenre.Items.AddRange(new object[] {
+            "Comedy",
+            "Thriller",
+            "Action",
+            "Drama"});
+            this.chkGenre.Location = new System.Drawing.Point(101, 374);
+            this.chkGenre.Name = "chkGenre";
+            this.chkGenre.Size = new System.Drawing.Size(120, 64);
+            this.chkGenre.TabIndex = 16;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 19);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(69, 17);
+            this.radioButton4.TabIndex = 17;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Animated";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(81, 19);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(93, 17);
+            this.radioButton5.TabIndex = 18;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Motion Picture";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // txtBudget
+            // 
+            this.txtBudget.Location = new System.Drawing.Point(122, 117);
+            this.txtBudget.Name = "txtBudget";
+            this.txtBudget.Size = new System.Drawing.Size(178, 20);
+            this.txtBudget.TabIndex = 20;
+            this.txtBudget.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            this.txtBudget.Validating += new System.ComponentModel.CancelEventHandler(this.txtBudget_Validating);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "HH:mm";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(122, 155);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(86, 20);
+            this.dateTimePicker1.TabIndex = 21;
+            this.dateTimePicker1.UseWaitCursor = true;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 7, 23, 1, 0, 0, 0);
+            // 
+            // grpFormats
+            // 
+            this.grpFormats.Controls.Add(this.radioButton4);
+            this.grpFormats.Controls.Add(this.radioButton5);
+            this.grpFormats.Location = new System.Drawing.Point(101, 198);
+            this.grpFormats.Name = "grpFormats";
+            this.grpFormats.Size = new System.Drawing.Size(200, 45);
+            this.grpFormats.TabIndex = 22;
+            this.grpFormats.TabStop = false;
+            this.grpFormats.Text = "Formats";
+            this.grpFormats.Validating += new System.ComponentModel.CancelEventHandler(this.grpFormats_Validating);
+            // 
+            // grpTypes
+            // 
+            this.grpTypes.Controls.Add(this.radioButton1);
+            this.grpTypes.Controls.Add(this.radioButton2);
+            this.grpTypes.Controls.Add(this.radioButton3);
+            this.grpTypes.Location = new System.Drawing.Point(101, 55);
+            this.grpTypes.Name = "grpTypes";
+            this.grpTypes.Size = new System.Drawing.Size(200, 44);
+            this.grpTypes.TabIndex = 23;
+            this.grpTypes.TabStop = false;
+            this.grpTypes.Text = "Types";
+            this.grpTypes.Validating += new System.ComponentModel.CancelEventHandler(this.grpTypes_Validating);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(278, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 40);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "View All Movies";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProviderApp
+            // 
+            this.errorProviderApp.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 509);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grpTypes);
+            this.Controls.Add(this.grpFormats);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtBudget);
+            this.Controls.Add(this.chkGenre);
+            this.Controls.Add(this.drpLanguage);
+            this.Controls.Add(this.txtDescription);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMovieName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.grpFormats.ResumeLayout(false);
+            this.grpFormats.PerformLayout();
+            this.grpTypes.ResumeLayout(false);
+            this.grpTypes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderApp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMovieName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.ComboBox drpLanguage;
+        private System.Windows.Forms.CheckedListBox chkGenre;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.TextBox txtBudget;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox grpFormats;
+        private System.Windows.Forms.GroupBox grpTypes;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProviderApp;
+    }
+}
+
